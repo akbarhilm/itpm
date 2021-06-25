6d11f089672525ea113863ba0c815ab3ec290c97
Akbar Hilman <ahilman@indonesian-aerospace.com>
2021-06-25 14:34:53 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
     script: "./index.js",
     instances : "max",
     exec_mode : "cluster",
+     max_memory_restart: '300M',
     env: {
       NODE_ENV: "development",
     },
