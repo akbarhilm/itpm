5fed390fda19c8b510aab97bb258e80cedfc59d9
akbar <ahilman@indonesian-aerospace.com>
2021-08-24 11:13:50 +0700

    get karyawan

--- controller/pengguna.js
+++ controller/pengguna.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const pengguna = require('../services/pengguna');
+const kar = require('./proyek')
+const http = require('https');
 
 router.get('/pengguna/nik', async (req, res, next) => {
     try {
@@ -63,5 +65,64 @@ router.get('/pengguna', async (req, res, next) => {
         next(err)
     }
 })
+
+function getinfonik(){
+    return new Promise(async(resolve,reject)=>{
+    
+    let rawData = ''
+        const options = new URL(`https://helpdesk-api.indonesian-aerospace.com/general/employee`);
+        await http.get(options, (res) => {
+        const { statusCode } = res;
+        const contentType = res.headers['content-type'];
+      
+        let error;
+        // Any 2xx status code signals a successful response but
+        // here we're only checking for 200.
+        if (statusCode !== 200) {
+          error = new Error('Request Failed.\n' +
+                            `Status Code: ${statusCode}`);
+        } else if (!/^application\/json/.test(contentType)) {
+          error = new Error('Invalid content-type.\n' +
+                            `Expected application/json but received ${contentType}`);
+        }
+        if (error) {
+          console.error(error.message);
+          // Consume response data to free up memory
+          res.resume();
+          return;
+        }
+      
+        res.setEncoding('utf8');
+       
+        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('end', () => {
+          try {
+            const parsedData = JSON.parse(rawData);
+           // console.log(parsedData);
+            resolve(parsedData)
+          } catch (e) {
+              reject(e)
+            console.error(e.message);
+          }
+        });
+      }).on('error', (e) => {
+        console.error(`Got error: ${e.message}`);
+      });
+
+      
+    })
+}
+
+router.get('/karyawan',async(req,res,next)=>{
+try{
+   const kar = await getinfonik()
+   
+   res.status(200).json(kar.data)
+}catch(e){
+    console.err(e)
+    res.status(500).json({"code":"99","message":"internal error"})
+}
+})
+
 
 module.exports = router
