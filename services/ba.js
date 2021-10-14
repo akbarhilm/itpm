bb95ae3e31e25726140c935555fb5a804f33df2e
akbar <ahilman@indonesian-aerospace.com>
2021-10-14 11:14:17 +0700

    fix ba

--- services/ba.js
+++ services/ba.js
@@ -4,9 +4,9 @@ const oracledb = require('oracledb');
 async function addBa(params){
 
     let query = `update dbadmit.tmitpmproj set
-        i_itpm_banbr =   select trim(to_char(nvl(nomer,'1'),'000'))||'/BAST/IT0000/'||to_char(sysdate,'mm')||'/'||to_char(sysdate,'yyyy') as nomer from(
+        i_itpm_banbr =   (select trim(to_char(nvl(nomer,'1'),'000'))||'/BAST/IT0000/'||to_char(sysdate,'mm')||'/'||to_char(sysdate,'yyyy') as nomer from(
             select trim(to_char(max(substr(i_itpm_banbr ,0,3))+1,'000')) as nomer from dbadmit.tmitpmproj where substr(i_itpm_banbr,-4) = to_char(sysdate,'yyyy')
-            ),
+            )),
         d_itpm_ba = sysdate,
         c_itpm_baapprv = 0
         where i_itpm_proj = :idproj
